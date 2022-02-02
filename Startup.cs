f253d82347e5105b5fcc0cbfb65743c734ef3fcb
Jovelino José Alves da Silva <jovelino.silva@pli-petronas.com>
2022-02-02 17:49:24 -0300

    teste

--- Startup.cs
+++ Startup.cs
@@ -21,8 +21,8 @@ namespace MyTodo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
 
             app.UseCors(option => option.AllowAnyOrigin());
 
